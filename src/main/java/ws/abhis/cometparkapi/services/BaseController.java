816cf4149dac5ac329dbf2ec30a09cef8ca430a7
Abhishek <abhishek@abhis.ws>
2014-04-13 22:44:58 -0400

    Added service for fetching markers based on parking lot.

--- src/main/java/ws/abhis/cometparkapi/services/BaseController.java
+++ src/main/java/ws/abhis/cometparkapi/services/BaseController.java
@@ -16,25 +16,19 @@ import ws.abhis.cometparkapi.logic.DatabaseConnect;
 @Controller
 @RequestMapping("/")
 public class BaseController {
-	private static final Logger logger = LogManager.getLogger(BaseController.class.getName());
+	private static final Logger logger = LogManager
+			.getLogger(BaseController.class.getName());
 	DatabaseConnect objDatabaseConnect = new DatabaseConnect();
-	
-	@RequestMapping(value = "/searchspaces/{lotId}/all", method = RequestMethod.GET)
-	public String searchSpaces(ModelMap model, @PathVariable String lotId) {
-		//Get centroid lat lon
-		List<List<String>> centroidLatLon = new ArrayList<>();
-		centroidLatLon = objDatabaseConnect.runSqlQuery("select lat, lon from tbl_parkinglots where lotid='" + lotId + "'");
-		model.addAttribute("centroidlat", centroidLatLon.get(0).get(0));
-		model.addAttribute("centroidlon", centroidLatLon.get(0).get(1));
-		
-		List<List<String>> spacesList = new ArrayList<>();
-		spacesList = objDatabaseConnect.runSqlQuery("select id, lat, lon, leveltype, imageloc from tbl_parkingspaces where lotid='" + lotId + "'");
-		
+
+	@RequestMapping(value = "/searchspaces/{lotId}/markers", method = RequestMethod.GET)
+	public String getAllMarkers(ModelMap model, @PathVariable String lotId) {
+		List<List<String>> getMarkers = new ArrayList<>();
+		getMarkers = objDatabaseConnect.runSqlQuery("select id, lat, lon, leveltype, imageloc from tbl_parkingspaces where lotid='" + lotId + "'");
 		String ret = "";
 		
-		if (spacesList.size() > 0) {
-			for (int i=0; i<spacesList.size(); i++) {
-				String lT = spacesList.get(i).get(3);
+		if (getMarkers.size() > 0) {
+			for (int i=0; i<getMarkers.size(); i++) {
+				String lT = getMarkers.get(i).get(3);
 				String vDesc = "";
 				if (lT.equals("ge")) {
 					vDesc = "Green parking permit.";
@@ -47,25 +41,71 @@ public class BaseController {
 				}
 				
 				
-				ret += "['" + spacesList.get(i).get(0) + "', " + spacesList.get(i).get(1) + ", " + spacesList.get(i).get(2) + ", '" + lT + "', '" + vDesc + "', '" + spacesList.get(i).get(4) + "'],";
+				ret += "['" + getMarkers.get(i).get(0) + "', " + getMarkers.get(i).get(1) + ", " + getMarkers.get(i).get(2) + ", '" + lT + "', '" + vDesc + "', '" + getMarkers.get(i).get(4) + "'],";
 			}
+		}
 			ret = ret.substring(0, ret.length()-1);
+			model.addAttribute("resp", ret);
+			
+			return "vanilla";
+	}
+
+	@RequestMapping(value = "/searchspaces/{lotId}/all", method = RequestMethod.GET)
+	public String searchSpaces(ModelMap model, @PathVariable String lotId) {
+		// Get centroid lat lon
+		List<List<String>> centroidLatLon = new ArrayList<>();
+		centroidLatLon = objDatabaseConnect
+				.runSqlQuery("select lat, lon from tbl_parkinglots where lotid='"
+						+ lotId + "'");
+		model.addAttribute("centroidlat", centroidLatLon.get(0).get(0));
+		model.addAttribute("centroidlon", centroidLatLon.get(0).get(1));
+
+		List<List<String>> spacesList = new ArrayList<>();
+		spacesList = objDatabaseConnect
+				.runSqlQuery("select id, lat, lon, leveltype, imageloc from tbl_parkingspaces where lotid='"
+						+ lotId + "'");
+
+		String ret = "";
+
+		if (spacesList.size() > 0) {
+			for (int i = 0; i < spacesList.size(); i++) {
+				String lT = spacesList.get(i).get(3);
+				String vDesc = "";
+				if (lT.equals("ge")) {
+					vDesc = "Green parking permit.";
+				} else if (lT.equals("pu")) {
+					vDesc = "Purple parking permit.";
+				} else if (lT.equals("go")) {
+					vDesc = "Gold parking permit.";
+				} else if (lT.equals("or")) {
+					vDesc = "Orange parking permit.";
+				}
+
+				ret += "['" + spacesList.get(i).get(0) + "', "
+						+ spacesList.get(i).get(1) + ", "
+						+ spacesList.get(i).get(2) + ", '" + lT + "', '"
+						+ vDesc + "', '" + spacesList.get(i).get(4) + "'],";
+			}
+			ret = ret.substring(0, ret.length() - 1);
 			model.addAttribute("latlonpairs", ret);
 		}
-		
+
 		return "searchspaces";
 	}
-	
-	
+
 	@RequestMapping(value = "/welcome", method = RequestMethod.GET)
 	public String welcomeM(ModelMap model) {
-		List<List<String>> retDb = objDatabaseConnect.runSqlQuery("select lat,lon,lotname,numid,lotid from tbl_parkinglots");
+		List<List<String>> retDb = objDatabaseConnect
+				.runSqlQuery("select lat,lon,lotname,numid,lotid from tbl_parkinglots");
 		String ret = "";
 		if (retDb.size() > 0) {
-			for (int i=0; i<retDb.size(); i++) {
-				ret += "['" + retDb.get(i).get(2) + "', " + retDb.get(i).get(0) + ", " + retDb.get(i).get(1) + ", " + retDb.get(i).get(3) + ", '" + retDb.get(i).get(4) +  "'],";
+			for (int i = 0; i < retDb.size(); i++) {
+				ret += "['" + retDb.get(i).get(2) + "', " + retDb.get(i).get(0)
+						+ ", " + retDb.get(i).get(1) + ", "
+						+ retDb.get(i).get(3) + ", '" + retDb.get(i).get(4)
+						+ "'],";
 			}
-			ret = ret.substring(0, ret.length()-1);
+			ret = ret.substring(0, ret.length() - 1);
 			model.addAttribute("latlonpairs", ret);
 			return "index";
 		}
